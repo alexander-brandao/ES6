4232215ea4500c626373b31a93b4b70d94190db0
Alexander Brandao <alex12a7x@gmail.com>
2022-02-09 08:51:46 -0300

    parametros default

---
+++ parametros_default.js
@@ -0,0 +1,5 @@
+function mensagem(nome, adjetivo, caracteristica = 'teimoso'){
+    document.write(`Era uma vez um cachorro chamado ${nome}, ele era muito ${adjetivo} e ${caracteristica}`)
+
+}
+mensagem('Loki', 'bonito', undefined)
