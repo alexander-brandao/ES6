4266bb66ce728ffb4fe80cbb5aca27d891308160
AlexLinux <alex12a7x@gmail.com>
2022-03-06 19:21:28 -0300

    o retorno do Node

---
+++ nodeJs/modolos/calculadora.js
@@ -0,0 +1,9 @@
+var funcSoma = require("./soma")
+
+var funcDiv = require("./divisao")
+
+var funcMulti = require("./multiplicacao")
+
+var funcSub = require("./subtracao")
+
+console.log(funcSub(1243,43))
